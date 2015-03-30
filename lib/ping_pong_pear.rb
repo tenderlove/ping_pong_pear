92c1a1c6a672d79e19d320253ba20d86a5d2580a
Aaron Patterson <aaron.patterson@gmail.com>
2015-03-30 16:51:53 -0700

    switch to dnssd

--- lib/ping_pong_pear.rb
+++ lib/ping_pong_pear.rb
@@ -1,143 +1,157 @@
-require 'socket'
-require 'ipaddr'
-require 'json'
+require 'dnssd'
+require 'webrick'
+require 'securerandom'
+require 'set'
+require 'net/http'
+require 'logger'
+require 'shellwords'
 
-module PingPongPear
-  VERSION        = '1.0.0'
-  UDP_PORT       = 4545
-  MULTICAST_ADDR = "224.0.0.1"
+class PingPongPear
+  SERVICE = "_http._tcp,pingpongpear"
 
-  class Broadcaster
-    def self.my_public_address
-      Socket.ip_address_list.reject { |addr|
-        addr.ipv4_loopback? || addr.ipv6_loopback? || addr.ipv6_linklocal?
-      }.first
+  def self.run args
+    new.public_send args.first, *args.drop(1)
+  end
+
+  attr_reader :pull_requests, :peers, :logger
+
+  def initialize
+    @pull_requests      = Queue.new
+    @send_pull_requests = Queue.new
+    @peers              = Set.new
+    @logger             = Logger.new $stdout
+  end
+
+  def start name = File.basename(Dir.pwd)
+    post_commit_hook = '.git/hooks/post-commit'
+    pidfile = '.git/pingpongpear.pid'
+
+    if File.exist? pidfile
+      raise "Another instance of Ping Pong Pear is running"
+    else
+      File.open(pidfile, 'w') { |f| f.write $$ }
     end
 
-    def self.send_update args
-      new.send ['commit'] + args
-    end
+    File.open(post_commit_hook, 'w') { |f|
+      f.write <<-eof
+#!/bin/sh
 
-    def self.send_location name, address, port
-      new.send ['source', name, address, port]
-    end
+git update-server-info
+kill -INFO $(cat #{pidfile})
+      eof
+    }
+    File.chmod 0755, post_commit_hook
 
-    def self.where_is? name
-      listener_t = Thread.new {
-        listener = Listener.new
-        listener.start { |cmd, n, *rest|
-          break(rest) if cmd == 'source' && n == name
-        }
-      }
-      broadcast = new
-      while listener_t.alive?
-        broadcast.send ['locate', name]
+    system "git update-server-info"
+
+    at_exit {
+      File.unlink pidfile
+      File.unlink post_commit_hook
+    }
+
+    identifier    = make_ident name
+
+    logger.debug "MY PROJECT NAME: #{name} IDENT: #{identifier}"
+
+    server        = start_server pull_requests
+    http_port     = server.listeners.map { |x| x.addr[1] }.first
+    hostname      = Socket.gethostname
+
+    discover identifier, name, peers
+    process_pull_requests pull_requests
+    process_send_pull_requests @send_pull_requests
+
+    trap('INFO') { send_pull_requests peers, hostname, http_port }
+
+    advertise(identifier, name, hostname, http_port).each { |x| x }
+  end
+
+  def clone name, dir = nil
+    browser = DNSSD::Service.browse SERVICE
+    browser.each do |response|
+      r = response.resolve
+      if r.text_record['project'] == name
+        url = "http://#{r.target}:#{r.port}"
+        system "git clone #{Shellwords.escape(url)} #{dir || Shellwords.escape(name)}"
+        break
       end
-      addr, port = listener_t.value
-      "http://#{addr}:#{port}/"
-    end
-
-    def initialize
-      @multicast_addr = MULTICAST_ADDR
-      @port           = UDP_PORT
-      @socket         = UDPSocket.open
-      @socket.setsockopt :IPPROTO_IP, :IP_MULTICAST_TTL, 1
-    end
-
-    def send message
-      @socket.send(JSON.dump(message), 0, @multicast_addr, @port)
     end
   end
 
-  class Listener
-    def initialize
-      @multicast_addr = MULTICAST_ADDR
-      @bind_addr      = "0.0.0.0"
-      @port           = UDP_PORT
+  private
 
-      @socket = UDPSocket.new
-      membership = IPAddr.new(@multicast_addr).hton + IPAddr.new(@bind_addr).hton
-
-      @socket.setsockopt :IPPROTO_IP, :IP_ADD_MEMBERSHIP, membership
-      @socket.setsockopt :SOL_SOCKET, :SO_REUSEPORT, 1
-
-      @socket.bind @bind_addr, @port
-    end
-
-    def start
-      loop do
-        message, _ = @socket.recvfrom 1024
-        yield JSON.parse(message)
+  def start_server pull_requests
+    server = WEBrick::HTTPServer.new Port: 0,
+                                     DocumentRoot: '.git',
+                                     Logger: logger
+    server.mount_proc '/pull' do |req, res|
+      host = req.query['host']
+      port = req.query['port']
+      if host && port
+        logger.info "ADDED PR: #{host}:#{port}"
+        pull_requests << [host, port.to_i]
       end
+      res.body = ''
     end
+    Thread.new { server.start }
+    server
   end
 
-  module Commands
-    def self.start args
-      require 'webrick'
-      require 'securerandom'
-      require 'shellwords'
+  def advertise ident, name, hostname, http_port
+    txt = DNSSD::TextRecord.new 'project' => name
+    DNSSD::Service.register ident, SERVICE, nil, http_port, hostname, txt
+  end
 
-      project_name     = args.first || File.basename(Dir.pwd)
-      post_commit_hook = '.git/hooks/post-commit'
-      uuid             = SecureRandom.uuid
+  def make_ident name
+    "#{name} (#{SecureRandom.hex.slice(0, 4)})"
+  end
 
-      system 'git update-server-info'
-
-      server = WEBrick::HTTPServer.new Port: 0, DocumentRoot: '.git'
-      http_port = server.listeners.map { |x| x.addr[1] }.first
-      my_address = Broadcaster.my_public_address.ip_address
-
-      File.open(post_commit_hook, 'w') { |f|
-        f.puts "#!/usr/bin/env ruby"
-        f.puts "ARGV[0] = 'update'"
-        f.puts "ARGV[1] = '#{project_name}'"
-        f.puts "ARGV[2] = '#{uuid}'"
-        f.puts "ARGV[3] = '#{my_address}'"
-        f.puts "ARGV[4] = '#{http_port}'"
-        f.write File.read __FILE__
-      }
-      File.chmod 0755, post_commit_hook
-
-      Thread.new {
-        listener = PingPongPear::Listener.new
-        listener.start { |cmd, name, *rest|
-          next unless name == project_name
-
-          case cmd
-          when 'locate' then Broadcaster.send_location project_name, my_address, http_port
-          when 'commit'
-            unless rest.first == uuid
-              url = "http://#{rest.drop(1).join(':')}"
-              system "git pull #{Shellwords.escape(url)}"
-            end
+  def discover ident, name, peers
+    browser = DNSSD::Service.browse SERVICE
+    browser.async_each do |response|
+      if response.flags.to_i > 0
+        logger.debug "SAW: #{response.name}"
+        unless response.name == ident
+          r = response.resolve
+          if r.text_record['project'] == name
+            logger.info "PEER: #{response.name}"
+            peers << [response.name, r.target, r.port]
           end
-        }
-      }
-
-      trap('INT') {
-        File.unlink post_commit_hook
-        server.shutdown
-      }
-      server.start
+        end
+      else
+        peers.delete_if { |id, _, _| id == response.name }
+        logger.info "REMOVED: #{response.name}"
+      end
     end
+  end
 
-    def self.clone args
-      require 'shellwords'
-      name = args.first
-      url = PingPongPear::Broadcaster.where_is? name
-      system "git clone #{Shellwords.escape(url)} #{Shellwords.escape(name)}"
+  def send_pull_requests peers, http_host, http_port
+    peers.each do |_, host, port|
+      @send_pull_requests << [host, port, http_host, http_port]
     end
+  end
 
-    def self.update args
-      system 'git update-server-info'
-      PingPongPear::Broadcaster.send_update args
+  def process_send_pull_requests requests
+    Thread.new do
+      while pr = requests.pop
+        host, port, http_host, http_port = *pr
+        http    = Net::HTTP.new host, port
+        request = Net::HTTP::Post.new '/pull'
+        request.set_form_data 'host' => http_host, 'port' => http_port
+        http.request request
+      end
     end
+  end
 
-    def self.run args
-      send args.first, args.drop(1)
+  def process_pull_requests pull_requests
+    Thread.new do
+      while pr = pull_requests.pop
+        url = "http://#{pr.join(':')}"
+        logger.debug "git pull #{Shellwords.escape(url)}"
+        system "git pull #{Shellwords.escape(url)}"
+      end
     end
   end
 end
 
-PingPongPear::Commands.run(ARGV) if $0 == __FILE__
+PingPongPear.run(ARGV) if $0 == __FILE__
